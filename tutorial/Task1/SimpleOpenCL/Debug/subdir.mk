94d76719cf19ff8170d2db6d1b6c794ddbd94aa1
Tobias Kenter <kenter@uni-paderborn.de>
2018-10-22 20:15:15 +0200

    Add project for tutorial task 1

---
+++ tutorial/Task1/SimpleOpenCL/Debug/subdir.mk
@@ -0,0 +1,27 @@
+################################################################################
+# Automatically-generated file. Do not edit!
+################################################################################
+
+# Add inputs and outputs from these tool invocations to the build variables 
+CPP_SRCS += \
+../main.cpp \
+../utility.cpp 
+
+OBJS += \
+./main.o \
+./utility.o 
+
+CPP_DEPS += \
+./main.d \
+./utility.d 
+
+
+# Each subdirectory must supply rules for building sources it contributes
+%.o: ../%.cpp
+	@echo 'Building file: $<'
+	@echo 'Invoking: GCC C++ Compiler'
+	g++ -std=c++0x -I/opt/intelFPGA_pro/17.1/hld/host/include -O0 -g3 -Wall -c -fmessage-length=0 -MMD -MP -MF"$(@:%.o=%.d)" -MT"$(@)" -o "$@" "$<"
+	@echo 'Finished building: $<'
+	@echo ' '
+
+
---
+++ tutorial/Task1/opencl_init.orig.sh
@@ -0,0 +1,6 @@
+export LM_LICENSE_FILE=/opt/intelFPGA_pro/IntelFPGA_license.dat
+export QUARTUS_ROOTDIR=/opt/intelFPGA_pro/17.1/quartus
+export INTELFPGAOCLSDKROOT=/opt/intelFPGA_pro/17.1/hld
+export LD_LIBRARY_PATH=/opt/intelFPGA_pro/17.1/hld/linux64/lib:/opt/intelFPGA_pro/17.1/hld/host/linux64/lib:/opt/intelFPGA_pro/17.1/hld/board/a10_ref/linux64/lib
+export PATH=$PATH:/opt/intel/opencl-sdk/bin:/opt/intelFPGA_pro/17.1/hld/bin:/opt/intelFPGA_pro/17.1/hld/host/linux64/bin
+export CL_CONTEXT_EMULATOR_DEVICE_INTELFPGA=a10gx
