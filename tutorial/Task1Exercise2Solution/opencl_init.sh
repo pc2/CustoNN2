fbcc8349c7eb998fddb4ecedb977f34ad6271f8f
Tobias Kenter <kenter@uni-paderborn.de>
2018-10-30 16:29:04 +0100

    Commit partial solution to task 1, add new practical and research task 2

---
+++ tutorial/Task1Exercise2Solution/opencl_init.sh
@@ -0,0 +1,6 @@
+export LM_LICENSE_FILE="27000@kiso.uni-paderborn.de"
+export QUARTUS_ROOTDIR=/opt/intelFPGA_pro/17.1/quartus
+export INTELFPGAOCLSDKROOT=/opt/intelFPGA_pro/17.1/hld
+export LD_LIBRARY_PATH=/opt/intelFPGA_pro/17.1/hld/linux64/lib:/opt/intelFPGA_pro/17.1/hld/host/linux64/lib:/opt/intelFPGA_pro/17.1/hld/board/nalla_pcie/linux64/lib
+export PATH=$PATH:/opt/intel/opencl-sdk/bin:/opt/intelFPGA_pro/17.1/hld/bin:/opt/intelFPGA_pro/17.1/hld/host/linux64/bin
+export CL_CONTEXT_EMULATOR_DEVICE_INTELFPGA=p385a_sch_ax115
