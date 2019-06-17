95c6dcd1b266b5726406c16bcdd49d403604885c
Suprajith Suresh Hakathur <hakathur@campus.uni-paderborn.de>
2019-06-17 23:47:57 +0200

    Take stride into consideration for calculating the number of times a for loop runs

--- Performance_Model/Antlr4Script/OpsCalculator.py
+++ Performance_Model/Antlr4Script/OpsCalculator.py
@@ -20,20 +20,19 @@ import sys
 
 class CPrintListener(CListener):
  
-    block_no = 0
-    num_func = 0
-    expression_list = []
+ 
    
     val_start = 0
     val_until = 0
-
     val_diff = [1] * 50
      
- 
+    val_stride = 1
     
     inside_for_declaration = 0
     inside_for_condition = 0
   
+    inside_for_expession_no = 0 
+    
     current_loop = 0
     current_func = 0
 
@@ -50,16 +49,24 @@ class CPrintListener(CListener):
 		if myEpression.isdigit() :
 			myEpression = int(myEpression)
 			self.val_start = myEpression
-			#print(self.val_start)
+			#print("Val of val start is {}".format(self.val_start))
 
         if(self.inside_for_condition == 1 ) :
-	        if myEpression.isdigit() :
-			myEpression = int(myEpression)
-			self.val_until = myEpression 
-			#print(self.val_until) 
-        		self.val_diff[self.current_func]  = self.val_diff[self.current_func] * (self.val_until - self.val_start)    
-        		#print("Val diffs here is {}".format(self.val_diff[self.current_func]))     
-    
+		if(self.inside_for_expession_no == 1 ) :
+	        	if myEpression.isdigit() :
+				myEpression = int(myEpression)
+				self.val_until = myEpression 
+				#print("Val of val until is {}".format(self.val_until))
+				 
+		if(self.inside_for_expession_no == 2 ) :
+	        	if myEpression.isdigit() :
+				myEpression = int(myEpression)
+				self.val_stride = myEpression                  
+                 		self.inside_for_expession_no = 0
+ 
+ 
+                              
+    				 
 
  
  
@@ -68,21 +75,26 @@ class CPrintListener(CListener):
         
     def enterFunctionDefinition(self, ctx):
         self.current_func = self.current_func + 1        
-
+	self.val_stride = 1
  
-    def exitForCondition(self, ctx):
-	pass
+    def exitForCondition(self, ctx): 
+	self.val_diff[self.current_func]  = self.val_diff[self.current_func] * ((self.val_until - self.val_start)/self.val_stride)	 
+	self.val_stride = 1 
+	
+            
+ 	
 
 	
     
     def enterForDeclaration(self, ctx):
         self.inside_for_declaration = 1
         self.inside_for_condition  = 0 
+        self.inside_for_expession_no = 0
  
     def enterForExpression(self, ctx):
         self.inside_for_declaration = 0
         self.inside_for_condition  = 1 
-	 
+	self.inside_for_expession_no = self.inside_for_expession_no + 1
  
      
     def exitCompilationUnit(self, ctx):
